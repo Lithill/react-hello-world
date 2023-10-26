337ce8819374d24694c2d8bd80540fa721d60713
Lithill <rossanneh@gmail.com>
2023-10-26 16:45:39 +0100

    Add props to the functional component

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -8,7 +8,16 @@ import Hi from './components/Hi';
 function App() {
   return (
     <div className="App">
-      <Greet></Greet>
+      <Greet name="Ross" surname="Hamilton">
+        <p>This is a child props</p>
+        <p>And this is another</p>
+      </Greet>
+      <Greet name="Chris" surname="Mugridge">
+        <button>Action</button>
+      </Greet>
+      <Greet name="Connie" surname="Hamilgridge"></Greet>
+      <Welcome></Welcome>
+      <Welcome></Welcome>
       <Welcome></Welcome>
       <HelloJSX></HelloJSX>
       <Hi></Hi>
--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -2,6 +2,14 @@ import React from 'react'
 
 // This is a functional component
 
-const Greet = () => <h1>Functional Component</h1>
+const Greet = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <h1>Hello {props.name} {props.surname} (Functional Component)</h1>
+            {props.children}
+        </div>
+    )
+}
 
 export default Greet
